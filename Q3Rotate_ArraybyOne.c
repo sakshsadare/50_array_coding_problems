576267efb33f8f634115d199bbd2259ad6220d0c
Sakshi  Sadare <sakshisadare514@gmail.com>
2025-12-06 23:39:50 +0530

    Implement array rotation by one position

--- Q3Rotate_ArraybyOne.c
+++ Q3Rotate_ArraybyOne.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+
+void rotateByOne(int arr[], int n) {
+    if (n <= 1) return;
+
+    int last = arr[n-1];
+    for (int i = n-1; i > 0; i--) {
+        arr[i] = arr[i-1];
+    }
+    arr[0] = last;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    rotateByOne(arr, n);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
 

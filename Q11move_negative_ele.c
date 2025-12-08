e514ac5e624d480d530439b9124344d30fde0e4b
Sakshi  Sadare <sakshisadare514@gmail.com>
2025-12-08 22:18:21 +0530

    Correct element segregation in array
    Fix logic errors in segregating positive and negative elements.

--- Q11move_negative_ele.c
+++ Q11move_negative_ele.c
@@ -1,1 +1,34 @@
+#include<stdio.h>
+void segregateElements(int arr[], int n)
+{
+   int temp[n];
+    int index = 0;
+// copy all postive elements in temp array
+  for(int i=0;i<n;i++){
+    if(arr[i]>=0)
+{
+  temp[index++] = arr[i];
+}
+}
+for(int i=0;i<n;i++){
+  if(arr[i]<0);{
+temp[index++] = arr[i];
+}
+}
+for (int i = 0; i < n; i++) {
+        arr[i] = temp[i];
+    }
+}
+
+int main() {
+    int arr[] = {1, -1, 3, 2, -7, -5, 11, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    segregateElements(arr, n);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
 
